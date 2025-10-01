e64665d5b185fc952f5ada2efe09c2204efbbea3
dinosehy <ana.7421848@aluno.mg.gov.br>
2025-10-01 10:13:54 -0300

    000.py renatin

---
+++ 001.py
@@ -0,0 +1,24 @@
+
+from datetime import datetime
+ano_atual = datetime.now().year
+
+print("verificador de maior idade renatin" 
+" " )
+
+ano_nascimento = int(input("Digite o ano em que você nasceu: "))
+
+idade = ano_atual - ano_nascimento
+
+if idade == 18:
+    print(f"Você fará ou já fez 18 anos em {ano_atual}!")
+elif idade > 18:
+    anos_passados = idade - 18
+    print(f"Você já completou 18 anos")
+    print(f"Você tem {idade} anos em {ano_atual}.")
+   
+else:
+    anos_faltando = 18 - idade
+    print(f"Você ainda não tem 18 anos")
+    print(f"Você tem {idade} anos em {ano_atual}.")
+ 
+
