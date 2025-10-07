c16fbe4939b2a82bc086c00e421e95d1a8414031
kn07117 <kauanrodriguessilva652@gmail.com>
2025-10-07 09:27:15 -0300

    Create 003.py

---
+++ kauan/003.py
@@ -0,0 +1,9 @@
+valor1 =int
+valor2 = int
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+if valor1 < valor2: print ("O segundo valor é o maior")
+
+elif valor2 < valor1: print ("o primeiro valor e maior")
