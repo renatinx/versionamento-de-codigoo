a7bfeaf66eb02775fbb324883ab9d129cd1de465
renatinx <renatincruz122@gmail.com>
2025-10-08 07:25:51 -0300

    Update 001.py

--- Renato/001.py
+++ Renato/001.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
-# Pega o ano atual automaticamente
+
 ano_atual = datetime.now().year
 
-# Solicita o ano de nascimento ao usuário
+
 ano_nascimento = int(input("Digite o ano em que você nasceu: "))
 
-# Calcula a idade
+
 idade = ano_atual - ano_nascimento
 
-# Verifica se fará ou já fez 18 anos
+
 if idade == 18:
     print("Você faz 18 anos neste ano!")
 elif idade > 18:
