cb444dc9548d46ee9a11ef2bf884af57e808bbb6
renatinx <renatincruz122@gmail.com>
2025-10-08 07:25:58 -0300

    Update 003.py

--- Renato/003.py
+++ Renato/003.py
@@ -4,6 +4,6 @@ valor2 = int
 valor1 = int(input("Digite o primeiro valor: "))
 valor2 = int(input("Digite o segundo valor: "))
 
-if valor1 < valor2: print ("O segundo valor é o maior")
+if valor1 <= valor2: print ("O segundo valor é o maior")
 
 elif valor2 < valor1: print ("o primeiro valor e maior")
