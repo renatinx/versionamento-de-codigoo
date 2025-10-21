ea1671192102c7fecf7016e84cbeb58390e8bf88
kn07117 <kauanrodriguessilva652@gmail.com>
2025-10-21 11:22:45 -0300

    Update 007.py

--- kauan/007.py
+++ kauan/007.py
@@ -1,7 +1,7 @@
 lista_cidades = []
-
+while True :
 print("Lista inicial:", lista_cidades)
-for i in range(3):
+for i in range():
     nome_cidade = input(f"Digite o nome da cidade #{i + 1}: ")
     lista_cidades.append(nome_cidade)
     print("Lista atualizada:", lista_cidades)
