f54e321b0900d03d2aebb868d9e4365719c53b99
kn07117 <>
2025-10-22 09:53:20 -0300

    Update 007.py

--- kauan/007.py
+++ kauan/007.py
@@ -3,8 +3,8 @@ LIMITI = 10000
 lista_cidades = []
 
 print("Lista inicial:", lista_cidades)
-for i in range(10000):
-    while True 
+
+while tr
     nome_cidade = input(f"Digite o nome da cidade #{i + 1}: ")
     lista_cidades.append(nome_cidade)
     print("Lista atualizada:", lista_cidades)
